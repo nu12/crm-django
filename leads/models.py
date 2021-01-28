44ec545c1c2954a81b268878c9a26a588283012d
Alysson A. Costa <alysson.avila.costa@gmail.com>
2021-01-28 17:36:52 -0300

    Add Model manager notes

--- leads/models.py
+++ leads/models.py
@@ -11,5 +11,18 @@ class Lead(models.Model):
     # on_delete: CASCADE, SET_NULL, SET_DEFAULT
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.first_name
+
 class Agent(models.Model):
     user = models.OneToOneField("User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+# Using the manager
+# Lead.objects.create(first_name = 'A', last_name = 'B', age = 30)
+# Lead.objects.all()
+# Lead.objects.filter(first_name='A')
+# Lead.objects.filter(age__gt= 20)
+# Lead.objects.get(id=15)
