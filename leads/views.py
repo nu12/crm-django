56b8b78f3df602caa9147b6629e90baf8f9781d5
Alysson A. Costa <alysson.avila.costa@gmail.com>
2021-02-03 08:12:39 -0300

    Add context to the template

--- leads/views.py
+++ leads/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_page(request):
-    return render(request, "leads/home_page.html")
+    context = {
+        "name": "Joe",
+        "age": 35
+    }
+    return render(request, "leads/home_page.html", context)
