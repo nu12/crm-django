a2666b24e67ebc7aa12246dfd7e6cff386fee7fa
Alysson A. Costa <alysson.avila.costa@gmail.com>
2021-02-03 09:11:37 -0300

    Add create view with form

--- crm/urls.py
+++ crm/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from leads.views import lead_list, lead_detail
+from leads.views import lead_list, lead_detail, lead_create
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/', lead_list),
-    path('leads/<id>', lead_detail),
+    path('leads/create', lead_create),
+    path('leads/<int:id>', lead_detail),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Lead
+from django.shortcuts import render, redirect
+from .models import Lead, Agent
+from .forms import LeadForm
 
 # Create your views here.
 
@@ -11,8 +12,26 @@ def lead_list(request):
     return render(request, "leads/lead_list.html", context)
 
 def lead_detail(request, id):
-    
     context = {
-        "leads": Lead.objects.get(id=id)
+        "lead": Lead.objects.get(id=id)
     }
     return render(request, "leads/lead_detail.html", context)
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            new_lead = Lead.objects.create(
+                first_name = form.cleaned_data['first_name'],
+                last_name = form.cleaned_data['last_name'],
+                age = form.cleaned_data['age'],
+                agent = Agent.objects.first()
+            )
+            return redirect("/leads/{}".format(new_lead.id)) 
+
+    context = {
+        "form": form
+    }
+    return render(request, "leads/lead_create.html", context)
