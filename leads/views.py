75ad46c90f1b16a02b8fede5e8ead7e6ad94ffe1
Alysson A. Costa <alysson.avila.costa@gmail.com>
2021-02-05 15:52:01 -0300

    Add ModelForm

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import LeadForm, LeadModelForm
 
 # Create your views here.
 
@@ -18,18 +18,12 @@ def lead_detail(request, id):
     return render(request, "leads/lead_detail.html", context)
 
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
-            new_lead = Lead.objects.create(
-                first_name = form.cleaned_data['first_name'],
-                last_name = form.cleaned_data['last_name'],
-                age = form.cleaned_data['age'],
-                agent = Agent.objects.first()
-            )
-            return redirect("/leads/{}".format(new_lead.id)) 
+            form.save()
+            return redirect("/leads") 
 
     context = {
         "form": form
