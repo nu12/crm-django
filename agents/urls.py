bf2b8a2fcd3743395026b237d0db1515a153f891
Alysson A. Costa <alysson.avila.costa@gmail.com>
2021-02-16 16:16:42 -0300

    Add Agent Detail Update and Delete

--- agents/urls.py
+++ agents/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from .views import AgentListView, AgentCreateView
+from .views import AgentListView, AgentCreateView, AgentDetailView, AgentUpdateView, AgentDeleteView
 
 app_name="agents"
 
 urlpatterns = [
     path('', AgentListView.as_view(), name='list'),
+    path('<int:pk>', AgentDetailView.as_view(), name='detail'),
     path('create', AgentCreateView.as_view(), name='create'),
-    #path('<int:pk>', LeadDetailView.as_view(), name='detail'),
-    #path('<int:pk>/update', LeadUpdateView.as_view(), name='update'),
-    #path('<int:pk>/delete', LeadDeleteView.as_view(), name='delete'),
+    path('<int:pk>/update', AgentUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete', AgentDeleteView.as_view(), name='delete'),
 ]
